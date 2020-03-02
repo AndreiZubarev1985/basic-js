408b842a009c09edc3819f2127a70bcbac8565a3
Andrei <andreizubarev1985@gmail.com>
2020-03-02 17:25:56 +0300

    feat: my ext-rep solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,17 @@
 module.exports = function repeater(str, options) {
-    let result = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition + options.separator).repeat(options.repeatTimes-1) + str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition;
-    return result;
-};
+    if(options.addition === undefined && options.additionSeparator === undefined) {
+        if(options.separator === undefined) {
+         let result = (str+'+').repeat(options.repeatTimes-1) + str;
+         return result;
+        } else {
+     let result = (str + options.separator).repeat(options.repeatTimes-1) + str;
+     return result;
+ }
+    } else if(options.separator === undefined) {
+       let result = (str+'+').repeat(options.repeatTimes-1) + str;
+        return result;
+    } else {
+     let result = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition + options.separator).repeat(options.repeatTimes-1) + str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition;
+     return result;
+ }
+ };
